d2ef9dc10e37a88bbd65c8b56c8714efc2e7dcb8
Ivan <pifiko@ukr.net>
2022-11-12 14:26:01 +0300

    add Col

--- pages/politics/index.js
+++ pages/politics/index.js
@@ -1,9 +1,42 @@
-import style from './war.module.scss'
+import style from './politics.module.scss'
 
 const Politics = () => (
-    <div className="container">
-      <div className="wrapperPol">
-        .col
+    <div className={style.container}>
+      <div className={style.wrapperPol}>
+        <div className={style.columns}>
+          <div className={style.wrapperCol}>
+            <div className={style.title}>Колонки</div>
+            <button className={style.allCol}>Всі колонки</button>
+          </div>
+          <div className={style.pepolWrapp}>
+            <div className={style.pepol}>
+              <div className={style.wrapperFoTex}>
+                <img src="/pepool/Avatar.png" alt="Avatar" />
+                <div className={style.wrapperTex}>
+                  <div className={style.name}>Юлія Стахівська</div>
+                  <div className={style.position}>письменниця</div>
+                </div>
+              </div>
+              <h2 className = {style.des}>Жінок в часи небезпеки приваблюють сильні чоловіки</h2>
+              <div className={style.time}>04 серпня 15:15</div>
+            </div>
+              <hr />
+            <div className={style.pepol}>
+              <div className={style.wrapperFoTex}>
+                <img src="/pepool/Avatar.png" alt="Avatar" />
+                <div className={style.wrapperTex}>
+                  <div className={style.name}>Юлія Стахівська</div>
+                  <div className={style.position}>письменниця</div>
+                </div>
+              </div>
+              <h2 className = {style.des}>Жінок в часи небезпеки приваблюють сильні чоловіки</h2>
+              <div className={style.time}>04 серпня 15:15</div>
+            </div>
+              <hr />
+          </div>
+          <button className ={style.allCols}>Всі колонки</button>
+        </div>
+        <div className={style.politics}></div>
       </div>
     </div>
   );
